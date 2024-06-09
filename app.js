b67ea0d3e5c6c7de7de67f727327185ce042138f
Sávio Mendes de Queiroz <160498021+saviomendeesq@users.noreply.github.com>
2024-06-09 09:20:01 -0300

    Add files via upload
    Projeto do jogo "Descubra o número" finalizado!

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 alert("Boas-vindas ao jogo do número secreto!");
-let numeroSecreto = 98;
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 //Enquanto o chute não for igual ao número secreto
 while (chute != numeroSecreto) {
-    chute = prompt("Escolha um número de 1 a 100");
+    chute = prompt(`Escolha um número de 1 a ${numeroMaximo}`);
 {    //Se o chute for igual ao número secreto.
     if (chute == numeroSecreto) {
        break
     } else {
     if (chute > numeroSecreto){
-        alert(`O número secreto é maior que ${chute}`);
-    } else {
         alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
         }
     }   
     tentativas++;
 }}
-if (tentativas >1){
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você descobriu o número secreto é ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+/*if (tentativas >1){
     alert(`Você descobriu o número secreto é ${numeroSecreto} com ${tentativas} tentativas`);
 } else {
     alert(`Você descobriu o número secreto é ${numeroSecreto} com ${tentativas} tentativa`);
-}
+}*/
